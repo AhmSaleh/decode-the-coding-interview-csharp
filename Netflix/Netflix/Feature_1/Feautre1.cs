3a5caf35811546d9b28e9783948307fbe26443f2
ahmsaleh <ahmedsaleh166@gmail.com>
2022-09-14 13:56:50 +0200

    add Feature 1

---
+++ Netflix/Netflix/Feature_1/Feautre1.cs
@@ -0,0 +1,47 @@
+﻿namespace Netflix.Feature_1
+{
+    public static class Feautre1
+    {
+        public static Dictionary<int, List<string>> getAnagramDict(List<string> titles)
+        {
+            Dictionary<int, List<string>> map = new();
+
+            foreach (var title in titles)
+            {
+                if (!map.ContainsKey(getTitleBits(title)))
+                {
+                    map[getTitleBits(title)] = new();
+                }
+
+                map[getTitleBits(title)].Add(title);
+            }
+
+            return map;
+        }
+
+        private static int getTitleBits(string title)
+        {
+            int bits = 0;
+            int mask;
+            int c;
+
+            for (int i = 0; i < title.Length; i++)
+            {
+                c = title[i] - 'a';
+
+                mask = 1;
+                mask <<= c;
+
+                bits |= mask;
+            }
+
+            return bits;
+        }
+    }
+
+
+
+
+
+
+}
--- Netflix/Netflix/Program.cs
+++ Netflix/Netflix/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Netflix
+﻿using Netflix.Feature_1;
+
+namespace Netflix
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Feature 1 Run
+            var titles = new List<string> { "duel", "dule", "speed", "spede", "deul", "cars" };
+            var titlesDic = Feautre1.getAnagramDict(titles);
+
+            foreach (var kvp in titlesDic)
+            {
+                foreach (var title in kvp.Value)
+                {
+                    Console.Write($"{title} ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+
+
+
         }
     }
 }
