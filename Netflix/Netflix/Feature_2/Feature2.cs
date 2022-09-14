e9b1bf63726e60fcd1de2855a6e08759502dc21b
ahmsaleh <ahmedsaleh166@gmail.com>
2022-09-14 17:42:48 +0200

    add Feature 2

---
+++ Netflix/Netflix/Feature_2/Feature2.cs
@@ -0,0 +1,57 @@
+﻿namespace Netflix.Feature_2
+{
+    public static class Feature2
+    {
+        public static ListNode merge2SortedLinkedList(ListNode list1, ListNode list2)
+        {
+
+            ListNode dummy = new(-1);
+            ListNode prev = dummy;
+
+            while (list1 != null & list2 != null)
+            {
+                if (list1?.val < list2?.val)
+                {
+                    prev.next = list1;
+                    list1 = list1.next;
+                }
+                else if (list1?.val > list2?.val)
+                {
+                    prev.next = list2;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    prev.next = list1;
+                    list1 = list1.next;
+                    list2 = list2.next;
+                }
+
+                prev = prev.next;
+            }
+
+            if (list1 == null)
+                prev.next = list2;
+            else
+                prev.next = list1;
+
+            return dummy.next;
+        }
+        public static ListNode mergeKSortedLinkedList(List<ListNode> lists)
+        {
+            ListNode list = null;
+
+            if (lists.Count != 0)
+                list = lists[0];
+
+            for (int i = 1; i < lists.Count; i++)
+            {
+                list = merge2SortedLinkedList(list, lists[i]);
+            }
+
+            return list;
+        }
+
+
+    }
+}
--- Netflix/Netflix/Program.cs
+++ Netflix/Netflix/Program.cs
@@ -1,30 +1,51 @@
-﻿using Netflix.Feature_1;
-
-namespace Netflix
+﻿namespace Netflix
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             // Feature 1 Run
-            var titles = new List<string> { "duel", "dule", "speed", "spede", "deul", "cars" };
-            var titlesDic = Feautre1.getAnagramDict(titles);
+            #region MyRegion
+            //var titles = new List<string> { "duel", "dule", "speed", "spede", "deul", "cars" };
+            //var titlesDic = Feautre1.getAnagramDict(titles);
 
-            foreach (var kvp in titlesDic)
-            {
-                foreach (var title in kvp.Value)
-                {
-                    Console.Write($"{title} ");
-                }
+            //foreach (var kvp in titlesDic)
+            //{
+            //    foreach (var title in kvp.Value)
+            //    {
+            //        Console.Write($"{title} ");
+            //    }
 
-                Console.WriteLine();
-            }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            // Feature 2
+            #region Feature 2
+            //ListNode list1 = new(4);
+            //list1.next = new(7);
+
+            //ListNode list2 = new(1);
+            //list2.next = new(9);
 
 
+            //ListNode list3 = new(0);
+            //list3.next = new(6);
 
+            //ListNode result = Feature2.mergeKSortedLinkedList(new List<ListNode>() { list1, list2, list3 });
 
+            //while (result != null)
+            //{
+            //    Console.WriteLine(result.val);
+            //    result = result.next;
+            //} 
+            #endregion
 
 
         }
+
+
+
     }
 }
